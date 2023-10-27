14571984911836176dbd4c133e3847a3c6d93e1c
Amina Haris <ameenaharis7@gmail.com>
2023-10-27 18:34:16 +0530

    'modified'

---
+++ server/db/conn.js
@@ -0,0 +1,21 @@
+const {mongoClient, MongoClient} = require('mongodb');
+const Db = process.env.ATLAS_URI;
+const client = new MongoClient(Db,{useNewUrlParser: true, useUnifiedTopology:true});
+
+var _db;
+
+module.exports={
+    connectToserver: function(callback) {
+        client.connect(function(err,db){
+            if(db)
+            {
+                _db = db.db('employees');
+                console.log('Successfully connected to MongoDB');
+            }
+            return callback(err);
+        });
+    },
+    getDb: function(){
+        return _db;
+    }
+}
